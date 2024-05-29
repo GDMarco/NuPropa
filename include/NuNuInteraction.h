04bb4a9602c8a3ad7147c11826480789a210e50a
GDMarco <122996249+GDMarco@users.noreply.github.com>
2024-05-29 11:09:58 +0200

    rearrangment

---
+++ include/NuNuInteraction.h
@@ -0,0 +1,8 @@
+#ifndef NUNUINTERACTION_H
+#define NUNUINTERACTION_H
+
+#include "nunuinteraction/NeutrinoNeutrinoIneteraction.h"
+#include "nunuinteraction/NeutrinoAntineutrinoIneteraction.h"
+#include "nunuinteraction/Channels.h"
+
+#endif 
